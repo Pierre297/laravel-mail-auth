3f00495d5a8a8005091476eeb6a6ed33a744e40c
Pier <pierfrancesco297@gmail.com>
2022-02-14 16:56:44 +0100

    lista videogames

--- resources/js/components/VideogamesComponent.vue
+++ resources/js/components/VideogamesComponent.vue
@@ -1,14 +1,36 @@
 <template>
   <div id="videogames">
     <h1>videogames</h1>
+    <table border="1">
+      <tr>
+        <th>Title</th>
+        <th>Subtitle</th>
+        <th>Rating</th>
+      </tr>
+
+      <tr v-for="videogame in videogames" :key="videogame.id">
+        <td>{{ videogame.title }}</td>
+        <td>{{ videogame.subtitle }}</td>
+        <td>{{ videogame.rating }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 
 <script>
 export default {
+  data: function () {
+    return {
+      videogames: [],
+    };
+  },
+
   mounted() {
-    console.log("Hello world");
+    axios
+      .get("/api/events/list")
+      .then((r) => (this.videogames = r.data))
+      .catch((e) => console.error(e));
   },
 };
 </script>
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+// rotte autenticazione
 Route::post('/login', 'Auth\LoginController@login')->name('login');
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
 Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
 
+// rotte pagina
 Route::get('/', 'GuestController@home')->name('home');
+
+// rotte veu
+Route::get('/api/events/list', 'ApiController@getEvents')->name('api.events.list');
