8c7d21070aba23bb91612ee4ee48ed4fa02ccecd
Pier <pierfrancesco297@gmail.com>
2022-02-14 16:23:35 +0100

    componente videogiochi vue

---
+++ resources/js/components/VideogamesComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="videogames">
+    <h1>videogames</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  mounted() {
+    console.log("Hello world");
+  },
+};
+</script>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,5 +2,7 @@
 @section('content')
 
 <h1>Hello World</h1>
+
+<videogames-component></videogames-component>
     
 @endsection
